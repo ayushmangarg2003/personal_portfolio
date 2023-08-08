dfea6b50f3cb3aa35e3ac60e13e33279736a01d6
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-08-08 18:16:58 +0530

    resume link updated

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -5,7 +5,7 @@ const LINKEDIN = "https://www.linkedin.com/in/ayushmangarg/";
 const TWITTER = "https://twitter.com/AyushmanGarg4";
 const GITHUB = "https://github.com/ayushmangarg2003";
 const MAIL = "https://mail.google.com/mail/u/0/?fs=1&to=ayushmangarg929@gmail.com&tf=cm";
-const CV = "https://drive.google.com/file/d/1N8cepY8JzpBu1MXBFNoTiL-QNwvdESJ1/view?usp=sharing";
+const CV = "https://drive.google.com/file/d/1zDHizWT3ETbPqPW2NfmcjGordHGa8aBU/view";
 const DETAILS_1 = "Hi, My name is Ayushman Garg and I am a MERN stack Web developer. My passion for computers brought me to Maharaja Agrasen Institute of Technology from where I am pursuing my BTech in Information Technology. I specialize in creating websites using ReactJS and other front-end tools, but I am also a quick learner and able to adapt to new environments and technologies."
 const DETAILS_2 = " I was first introduced to programming in my 11th grade through Python programming language. I then began learning web development during my first year of college and have since gained experience working with technologies such as HTML, CSS, JavaScript, ReactJs, MongoDB, ExpressJs, NodeJs, Bootstrap, and Tailwind."
 
