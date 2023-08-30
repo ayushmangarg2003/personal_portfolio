24f0e1cf9a7f602df1b2c04be180389d1b699f48
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-08-30 19:02:54 +0530

    Update Helper.js

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -7,7 +7,7 @@ const GITHUB = "https://github.com/ayushmangarg2003";
 const MAIL = "https://mail.google.com/mail/u/0/?fs=1&to=ayushmangarg929@gmail.com&tf=cm";
 const CV = "https://drive.google.com/file/d/1zDHizWT3ETbPqPW2NfmcjGordHGa8aBU/view";
 const DETAILS_1 = "Hi, my name is Ayushman Garg, and I am a MERN stack web developer as well as a React Native developer. My passion for computers brought me to Maharaja Agrasen Institute of Technology, where I am pursuing my Bachelor's degree in Information Technology. I specialize in creating websites using the MERN stack, and I am also a quick learner, capable of adapting to new environments and technologies."
-const DETAILS_2 = "I was initially introduced to programming in school through the Python language. Subsequently, during my first year of college, I started learning web development. Since then, I have gained experience working with technologies like HTML, CSS, JavaScript, ReactJS, MongoDB, ExpressJS, NodeJS, MySQL, React Native, and various other frameworks.""
+const DETAILS_2 = "I was initially introduced to programming in school through the Python language. Subsequently, during my first year of college, I started learning web development. Since then, I have gained experience working with technologies like HTML, CSS, JavaScript, ReactJS, MongoDB, ExpressJS, NodeJS, MySQL, React Native, and various other frameworks."
 
 
 module.exports = {
