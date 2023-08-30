2e877368377bc59127416eb831830b9da98375db
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-08-30 19:01:00 +0530

    Update Helper.js

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -6,8 +6,8 @@ const TWITTER = "https://twitter.com/AyushmanGarg4";
 const GITHUB = "https://github.com/ayushmangarg2003";
 const MAIL = "https://mail.google.com/mail/u/0/?fs=1&to=ayushmangarg929@gmail.com&tf=cm";
 const CV = "https://drive.google.com/file/d/1zDHizWT3ETbPqPW2NfmcjGordHGa8aBU/view";
-const DETAILS_1 = "Hi, My name is Ayushman Garg and I am a MERN stack Web developer. My passion for computers brought me to Maharaja Agrasen Institute of Technology from where I am pursuing my BTech in Information Technology. I specialize in creating websites using ReactJS and other front-end tools, but I am also a quick learner and able to adapt to new environments and technologies."
-const DETAILS_2 = " I was first introduced to programming in my 11th grade through Python programming language. I then began learning web development during my first year of college and have since gained experience working with technologies such as HTML, CSS, JavaScript, ReactJs, MongoDB, ExpressJs, NodeJs, Bootstrap, and Tailwind."
+const DETAILS_1 = "Hi, my name is Ayushman Garg, and I am a MERN stack web developer as well as a React Native developer. My passion for computers brought me to Maharaja Agrasen Institute of Technology, where I am pursuing my Bachelor's degree in Information Technology. I specialize in creating websites using the MERN stack, and I am also a quick learner, capable of adapting to new environments and technologies."
+const DETAILS_2 = "I was initially introduced to programming in school through the Python language. Subsequently, during my first year of college, I started learning web development. Since then, I have gained experience working with technologies like HTML, CSS, JavaScript, ReactJS, MongoDB, ExpressJS, NodeJS, MySQL, React Native, and various other frameworks.""
 
 
 module.exports = {
